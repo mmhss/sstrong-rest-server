961aec481a095c828e3b6f2b333e7bc110c3fe6c
pbyanjankar <Prabin.Byanjankar@gmail.com>
2019-02-06 02:14:46 +0545

    #10 Add proximity-chart endpoint on mother resource

--- src/main/java/com/gates/standstrong/domain/mother/MotherResource.java
+++ src/main/java/com/gates/standstrong/domain/mother/MotherResource.java
@@ -1,11 +1,15 @@
 package com.gates.standstrong.domain.mother;
 
 import com.gates.standstrong.base.BaseResource;
+import com.gates.standstrong.domain.data.proximity.ProximityChart;
+import com.gates.standstrong.domain.data.proximity.ProximityService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = BaseResource.BASE_URL + MotherResource.RESOURCE_URL)
@@ -15,16 +19,28 @@ public class MotherResource extends BaseResource<Mother, MotherDto> {
 
     public MotherService motherService;
 
+    public ProximityService proximityService;
+
     @Inject
-    public MotherResource(MotherService motherService, MotherMapper motherMapper) {
+    public MotherResource(MotherService motherService, ProximityService proximityService, MotherMapper motherMapper) {
         super(motherService, motherMapper, Mother.class, QMother.mother);
         this.motherService = motherService;
+        this.proximityService = proximityService;
     }
 
     @GetMapping("/motherid")
     public Long motherId(){
 
         return motherService.getMotherId("SS1001-ui.txt", "-");
+
+    }
+
+    @GetMapping("/{id}/proximity-chart")
+    public List<ProximityChart> getProximityChart(@PathVariable Long id){
+
+        List<ProximityChart> result = proximityService.getProximityChart(id);
+
+        return result;
 
     }
 
