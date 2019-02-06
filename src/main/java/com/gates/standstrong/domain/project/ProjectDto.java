c0384c745283cf06747930d6518e05da433ca481
pbyanjankar <Prabin.Byanjankar@gmail.com>
2019-02-06 10:28:00 +0545

    Add json format for dates in project and award objects

--- src/main/java/com/gates/standstrong/domain/project/ProjectDto.java
+++ src/main/java/com/gates/standstrong/domain/project/ProjectDto.java
@@ -1,5 +1,6 @@
 package com.gates.standstrong.domain.project;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gates.standstrong.base.BaseDto;
 import lombok.Data;
 
@@ -10,7 +11,11 @@ public class ProjectDto extends BaseDto {
 
     private String name;
     private String location;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate completionDate;
 
 }
