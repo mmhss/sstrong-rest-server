bfad3e569fe207ab84e731febe2158ed066e568f
pbyanjankar <Prabin.Byanjankar@gmail.com>
2019-02-11 08:21:57 +0545

    #14 Add Social Support

--- src/main/java/com/gates/standstrong/domain/data/audio/AudioRepository.java
+++ src/main/java/com/gates/standstrong/domain/data/audio/AudioRepository.java
@@ -1,6 +1,31 @@
 package com.gates.standstrong.domain.data.audio;
 
 import com.gates.standstrong.base.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Date;
+import java.util.List;
 
 interface AudioRepository extends BaseRepository<Audio> {
+
+    @Query(value = "SELECT DISTINCT(date(capture_date)) as capture_date" +
+            " FROM audio WHERE mother_id = :motherId" +
+            " ORDER BY capture_date ", nativeQuery = true)
+    List<Date> getAudioCapturedDates(@Param("motherId") Long motherId);
+
+
+    @Query(value = "SELECT count(audio_type)" +
+            " FROM Audio WHERE mother_id = :motherId" +
+            " AND date(capture_date)=:awardForDate")
+    Long getTalkCount(@Param("motherId") Long motherId, @Param("awardForDate") Date awardForDate);
+
+
+    @Query(value = "SELECT DATE(capture_date) AS captureDate, COUNT(audio_type) as speechCount" +
+            " FROM audio" +
+            " WHERE mother_id = :motherId" +
+            " AND audio_type = 'Speech'" +
+            " GROUP BY DATE(capture_date)", nativeQuery = true)
+    List<Speech> getSpeechCount(@Param("motherId") Long motherId);
+
 }
--- src/main/java/com/gates/standstrong/domain/data/importfile/ImportFileRepository.java
+++ src/main/java/com/gates/standstrong/domain/data/importfile/ImportFileRepository.java
@@ -1,6 +1,14 @@
 package com.gates.standstrong.domain.data.importfile;
 
 import com.gates.standstrong.base.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 interface ImportFileRepository extends BaseRepository<ImportFile> {
+
+    @Query(value = "SELECT id FROM ImportFile WHERE ImportFile.mother_id = :motherId", nativeQuery = true)
+    List<ImportFile> getImportFilesByMotherId(@Param("motherId") Long motherId);
+
 }
