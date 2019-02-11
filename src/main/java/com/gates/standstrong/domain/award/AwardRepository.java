c79099599e8dfd3323721c6a35fbd80e2f4eca34
pbyanjankar <Prabin.Byanjankar@gmail.com>
2019-02-11 08:21:57 +0545

    #14 Add Social Support

--- src/main/java/com/gates/standstrong/domain/award/AwardRepository.java
+++ src/main/java/com/gates/standstrong/domain/award/AwardRepository.java
@@ -1,6 +1,17 @@
 package com.gates.standstrong.domain.award;
 
 import com.gates.standstrong.base.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 interface AwardRepository extends BaseRepository<Award> {
+
+    @Query(value = "SELECT * FROM award WHERE mother_id=:motherId and award_type=:awardType", nativeQuery = true)
+    List<Award> getAwards(@Param("motherId") Long motherId, @Param("awardType") String awardType);
+
+    @Query(value = "SELECT * FROM award WHERE mother_id=:motherId AND award_type=:awardType ORDER BY award_level DESC LIMIT 1", nativeQuery = true)
+    Award getTopAward(@Param("motherId") Long motherId, @Param("awardType") String awardType);
+
 }
--- src/main/java/com/gates/standstrong/domain/award/AwardService.java
+++ src/main/java/com/gates/standstrong/domain/award/AwardService.java
@@ -1,6 +1,15 @@
 package com.gates.standstrong.domain.award;
 
 import com.gates.standstrong.base.BaseService;
+import com.gates.standstrong.domain.mother.Mother;
+
+import java.util.List;
 
 public interface AwardService extends BaseService<Award> {
+
+    List<Award> getAwards(Long motherId, String awardType);
+
+    Award getTopAward(Long motherId, String awardType);
+
+    boolean hasHighestAward(Mother mother, String awardType);
 }
