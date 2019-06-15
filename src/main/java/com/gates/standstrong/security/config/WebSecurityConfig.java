a2a061b0c3fe7fe458c66504ff832f21aa57f6bb
pbyanjankar <Prabin.Byanjankar@gmail.com>
2019-06-15 07:48:12 +0545

    Add allowed headers in cors check

--- src/main/java/com/gates/standstrong/security/config/WebSecurityConfig.java
+++ src/main/java/com/gates/standstrong/security/config/WebSecurityConfig.java
@@ -75,9 +75,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
